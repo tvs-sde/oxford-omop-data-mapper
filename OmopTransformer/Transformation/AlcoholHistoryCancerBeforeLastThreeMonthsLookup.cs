06cf0dd5aead5a5924024004fee0dbf14d925f64
James Cockayne <150046545+james-cockayne@users.noreply.github.com>
2026-07-06 10:51:55 +0100

    Fix alcohol history concept

--- OmopTransformer/Transformation/AlcoholHistoryCancerBeforeLastThreeMonthsLookup.cs
+++ OmopTransformer/Transformation/AlcoholHistoryCancerBeforeLastThreeMonthsLookup.cs
@@ -10,7 +10,7 @@ internal class AlcoholHistoryCancerBeforeLastThreeMonthsLookup : ILookup
         {
             { "1", new ValueWithNote("4336673", "Heavy (greater than 14 units per week)") },
             { "2", new ValueWithNote("4042862", "Light (less than or equal to 14 units per week)") },
-            { "3", new ValueWithNote("105542008", "None ever") },
+            { "3", new ValueWithNote("4022664", "None ever") },
             { "Z", new ValueWithNote("0", "Not Stated (patient asked but declined to provide a response)") },
             { "9", new ValueWithNote("0", "Not Known (Not Recorded)") },
         };
